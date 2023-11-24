edc4d928c3cb1149ff06d3095a8539f46662cdec
gcf <gechaofeng@jiuxiniot.com>
2023-11-24 17:56:15 +0800

    swagger

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+  list: any[];
+
+  constructor() {
+    this.list = [];
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  findAll(): any {
+    return {
+      code: 200,
+      data: this.list,
+      msg: '获取成功',
+    };
+  }
+
+  @Post()
+  create() {
+    this.list.push(1);
+    return {
+      code: 200,
+      data: null,
+      msg: '创建成功',
+    };
   }
 }
